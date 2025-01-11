d01b12acae7e41678f0e0f66c2f61edba82c52df
Praveen <pkumarrajput498@gmail.com>
2025-01-11 16:00:06 +0530

    user save

--- EmailSystem/Controllers/UserController.cs
+++ EmailSystem/Controllers/UserController.cs
@@ -1,11 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmailSystem.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EmailSystem.Controllers
 {
     public class UserController : Controller
     {
+        public AppDbContext appDbContext;
+        public UserController(AppDbContext _AppDbContext)
+        {
+            appDbContext = _AppDbContext;
+        }
         public IActionResult Index()
         {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Users objUsers)
+        {
+            try
+            {
+                objUsers.createdOn = DateTime.Now;
+                appDbContext.Users.Add(objUsers);
+                RedirectToAction("Index", "Login");
+            }
+            catch (Exception ex)
+            {
+
+            }
+            
             return View();
         }
     }
