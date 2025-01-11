d589d135edfa5356779d3864447df6d22eb99ebe
Praveen <pkumarrajput498@gmail.com>
2025-01-11 13:31:19 +0530

    user controller added

---
+++ EmailSystem/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmailSystem.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
