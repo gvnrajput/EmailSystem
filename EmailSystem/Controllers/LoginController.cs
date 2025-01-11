05254ad51258d9dc35e569e0f5db36bbdb693096
Dell <Dell@DESKTOP-TMLM8DU>
2025-01-11 13:26:33 +0530

    Add project files.

---
+++ EmailSystem/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using EmailSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace EmailSystem.Controllers
+{
+    
+    public class LoginController : Controller
+    {
+        public AppDbContext appDbContext;
+        public LoginController(AppDbContext _AppDbContext)
+        {
+            appDbContext = _AppDbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(Users obj)
+        {
+           List<Users> users = appDbContext.Users.ToList();
+            Users usr = users.Where(x=> x.UserName == obj.UserName && x.password==obj.password).FirstOrDefault();
+            if (usr!=null)
+            {
+                RedirectToAction("Index", "Home");
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ EmailSystem/Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmailSystem.Models
+{
+    public class Users
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string password { get; set; }
+        public string mobile { get; set; }
+        public string email { get; set; }
+        public DateTime createdOn { get; set; }
+    }
+}
